435d1030e0845200686426e55a1af500542d4705
Samuel Warren <samuellhwarren@live.com>
2024-05-31 16:33:34 -0700

    playing with strings

--- Start.py
+++ Start.py
@@ -1,5 +1,28 @@
-print('Hello World')
+# print('Hello World')
 
-name = input('What is your name? ')
-print ('Hello')
-print (name)
+# name = input('What is your name? ')
+# print ('Hello')
+# print (name)
+# # comment
+#
+#
+#strings
+# first_name = 'Sam'
+# print(first_name)
+# last_name = 'Warren'
+# print ('Hello ' + first_name + ' ' + last_name)
+# sentence = 'The dog is named Sammy'
+# print(sentence.upper())
+# print(sentence.lower())
+# print(sentence.capitalize())
+# print(sentence.count('a'))
+# first_name = input('What is your first name? ')
+# last_name = input('What is your last name ?')
+# print('Hello ' + first_name.capitalize() + ' ' + last_name.capitalize())
+# first_name = 'Sam'
+# last_name ='Warren'
+first_name = input('Please Enter your First Name: ')
+last_name = input ('Please enter your Last Name: ')
+
+print(first_name + last_name)
+print('Hello, '+ first_name.capitalize() + ' ' + last_name.capitalize())
