341e6f7fa47b425ddb53c96acb54fefe542dcf25
Samuel Warren <samuellhwarren@live.com>
2024-06-07 13:50:35 -0700

    adding numbers training

--- Start.py
+++ Start.py
@@ -21,8 +21,23 @@
 # print('Hello ' + first_name.capitalize() + ' ' + last_name.capitalize())
 # first_name = 'Sam'
 # last_name ='Warren'
-first_name = input('Please Enter your First Name: ')
-last_name = input ('Please enter your Last Name: ')
+# first_name = input('Please Enter your First Name: ')
+# last_name = input ('Please enter your Last Name: ')
 
-print(first_name + last_name)
-print('Hello, '+ first_name.capitalize() + ' ' + last_name.capitalize())
+# print(first_name + last_name)
+# print('Hello, '+ first_name.capitalize() + ' ' + last_name.capitalize())
+
+
+# first_name = 'Samuel'
+# last_name = 'Warren'
+# output = f'Hello, {first_name} {last_name}'
+# print(output)
+# days_in_feb = 28
+# print(str(days_in_feb) + ' Days in February')
+
+# first_num = 5
+# second_num = 6
+# print(first_num ** second_num)
+# first_num = input('Please enter a number: ')
+# second_num = input('Please Enter another Number: ')
+# print(int(first_num) + int(second_num))
